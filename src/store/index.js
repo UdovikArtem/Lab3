4bad29e13491c9c81dd4bcd34cfdeb5d0e392b0b
UdovikArtem <artem.ud2003@example.com>
2023-12-17 20:49:42 +0300

    modified:   src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { createStore } from 'redux';
 // import {configureStore} from '@reduxjs/toolkit'
 // import reducer from '../reducers/userReduser';
 import {reducer} from '../reducers/userReduser'
-
-const rootReducer = combineReducers(reducer)
 
 export const store = createStore(reducer)
